17f98f81aa0e072ab0e1440eedf368b8bed1d113
Андрей <andrei@MacBook-Air-Andrej.local>
2026-07-15 13:50:55 +0300

    fix: try id.vk.com/oauth2/authorize endpoint

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -497,7 +497,7 @@ export async function signInVK() {
   const { verifier, challenge } = await _pkceChallenge();
   sessionStorage.setItem('vk_code_verifier', verifier);
 
-  const vkUrl = `https://id.vk.com/oauth2/auth?client_id=${VK_APP_ID}` +
+  const vkUrl = `https://id.vk.com/oauth2/authorize?client_id=${VK_APP_ID}` +
     `&redirect_uri=${encodeURIComponent(_vkRedirectUri())}` +
     `&response_type=code&scope=email&state=vk` +
     `&code_challenge=${challenge}&code_challenge_method=S256`;
